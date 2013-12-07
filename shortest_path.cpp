f4a21e8ad6b4934dccd381083e133adb0f48086d
James Powell <blackicec@gmail.com>
2013-12-07 02:23:26 -0500

    Begin initialization in Dijkstra algorithm. Next step is to somehow add node structures to the priority queue with a comparator that will sort their current path values. I may make a comparator and give the constructor the functor.

--- shortest_path.cpp
+++ shortest_path.cpp
@@ -17,20 +17,36 @@ typedef struct Edge {
 	char start_node,
 		end_node;
 	int weight;
+	bool visited;
 } Edge;
 
 typedef struct Node {
-
+	char id;
+	vector<char> adj_nodes;
 } Node;
 
+typedef struct PathNode {
+	char id;
+	int shortest_path;
+
+	char parent_id;	// previous node in shortest path that leads to this node
+} PathNode;
+
 // FUNCTIONS
-void process(char*);
+void process(char*);	// records edges, weights and nodes to form a graph
+
+// the PathNode array will include the shortest paths when this function completes
+void Dijkstra(char, vector< PathNode >& );
+void printGraph();
+
 // END FUNCTION DECLARATIONS
 
 // GLOBAL VARIABLES
 bool is_directed;			// set if the edges in the graph are directed
+int weight_sum;				// the sum of all weights in the original graph
 map< char, Node > graph; 	// the graph containing all the edges
 vector< Edge > edges; 		// This will keep a list of edges and there weights
+vector< PathNode > final_paths;
 std::priority_queue< int, std::vector<int>, std::greater<int> > heap;
 priority_queue<int, std::vector<int>, std::greater<int> > queue;
 // END GLOBAL DEFINITIONS
@@ -62,12 +78,17 @@ int main( int argc, char** argv) {
 
 	cout << "-> The file has been processed.\n";
 	process( filename );
+	printGraph();
 }
 
 void process(char* filename) {
 	char i, j;	// vertex1, vertex2
 	int w;		// weight of an edge
 	string extra_text;
+
+	// this will serve as our "infinity" for the initial
+	// shortest path weight
+	weight_sum = 0;
 
 	ifstream file;
 	file.open( filename );
@@ -92,17 +113,87 @@ void process(char* filename) {
 		e.start_node = i;
 		e.end_node = j;
 		e.weight = w;
+		e.visited = false;
 
 		edges.push_back( e );
 
-		
-	}
+		weight_sum += w;	// increase the sum of the 
 
+		// now build an adjancency list from the node parsed
+
+		// if inital vertex is not in the map then put it in and give it
+		// a node with its one connectivity node, j
+		if( it == graph.end() ) {
+			Node n;
+			n.id = i;
+
+			graph[i] = n;			// add the node with the id as the key
+
+			graph[i].adj_nodes.push_back(j);
+		}
+
+		// else, the key is already in the mapping, so add another
+		// connectivity node to its vector
+		else 
+			graph[i].adj_nodes.push_back(j);
+
+		// check to see if j is a key in the map
+		// if not, then add it
+		if( it2 == graph.end() ) {
+			Node terminal;
+			terminal.id = j;
+
+			graph[j] = terminal;
+		}
+
+		// if the file contains a '/n' between each line, clear it
+		if( file.peek() == '\n')	file.ignore();
+	}
 	file.close();
 
 	cout << "My edges:\n";
 
 	for(int i = 0; i < edges.size(); ++i)
-		cout << edges[i].start_node << " -> " << edges[i].end_node << " with weight = " << edges[i].weight << endl;
+		cout << edges[i].start_node << " -> " << edges[i].end_node 
+			<< " with weight = " << edges[i].weight << endl;
 }
 
+void Dijkstra(char source, vector<PathNode>& final_paths) {
+
+	// Initialize all the node's shortest paths to the sum of the weights
+	// since this is the maximum possible path from any node to another
+	// without forming a cycle
+	map< char, Node >::iterator it = graph.begin();	
+
+	for( ; it != graph.end(); ++it) {
+		// take every key in the map and initalize the vector holding the final
+		// paths.
+		PathNode node;
+		node.id = (char)it->first;
+		node.parent_id = '~';	// default parent node character
+		node.shortest_path = weight_sum;
+
+		final_paths.push_back( node );
+	} 
+}
+
+void printGraph() {
+
+	map< char, Node >::iterator it = graph.begin();	    
+
+	int vecSize = 0;
+
+	cout << "Original Graph\n";
+	for( ; it != graph.end(); ++it) {
+		cout << it->first << ": ";
+
+		vecSize = it->second.adj_nodes.size();
+			
+		// print adjacent nodes
+		for(int i = 0; i < vecSize; ++i) {
+			cout << it->second.adj_nodes[i] << ' ';
+		}
+
+		cout << endl;
+	}
+}
